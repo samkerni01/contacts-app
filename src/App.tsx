5c49fcd0e0386acc52200a63171f19f461f986b8
samkerniol <pavelkorol3v@gmail.com>
2022-04-09 18:53:46 +0300

    Create SignIn component

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './styles/globals.css';
+
+import SignIn from './components/SignIn/SignIn';
+
+export default function App() {
+	return <SignIn />;
+}
---
+++ src/components/SignIn/SignIn.tsx
@@ -0,0 +1,47 @@
+import email from './icons/email.svg';
+import lock from './icons/lock.svg';
+
+import styles from './SignIn.module.css';
+
+export default function SignIn() {
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const data = new FormData(e.currentTarget);
+
+		console.log({
+			email: data.get('email'),
+			password: data.get('password')
+		});
+	};
+
+	return (
+		<form className={styles.wrapper} onSubmit={handleSubmit}>
+			<div className={styles.title}>Welcome!</div>
+			<div className={styles.descr}>Enter your login details</div>
+
+			<div className={styles.text}>E-Mail</div>
+			<div className={styles.relative}>
+				<img className={styles.icon} src={email} alt="email" />
+				<input
+					className={styles.input}
+					type="text"
+					name="email"
+					placeholder="e.g.: elonmusk@mars.com"
+				/>
+			</div>
+
+			<div className={`${styles.text} ${styles.mt24}`}>Password</div>
+			<div className={styles.relative}>
+				<img className={styles.icon} src={lock} alt="lock" />
+				<input
+					className={styles.input}
+					type="text"
+					name="password"
+					placeholder="e.g.: xaea-12"
+				/>
+			</div>
+
+			<button className={styles.btn}>Sign In</button>
+		</form>
+	);
+}
